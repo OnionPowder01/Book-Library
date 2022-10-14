e9c8de37cea506204949c5503203af92efa93708
OnionPowder01 <autonatione36@gmail.com>
2022-10-14 21:44:09 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+let myLibrary = [];
+let i = 0;
+
+//Constructor
+
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.ShowContent = function () {
+    return ` Book title: ${this.title} \n Author: ${this.author} \n Number of pages: ${this.pages}`;
+  };
+}
+
+function ShowBook() {
+  const element = document.createElement("div");
+  const p = document.createElement("p");
+  const btn = document.createElement("button");
+  const btnRemove = document.createElement("button");
+
+  output = "";
+  output += myLibrary[i].ShowContent() + "; ";
+
+  element.classList.add(`MyClass${i}`);
+  p.innerHTML = output;
+
+  //Remove Button
+  btnRemove.innerText = "Delete";
+  btnRemove.addEventListener("click", function RemoveBook() {
+    element.outerHTML = "";
+  });
+
+  //Read Button
+
+  btn.innerText = "Not Read Yet";
+  btn.addEventListener("click", function ReadClick() {
+    btn.textContent = "Read";
+    btn.style.backgroundColor = "green";
+    btn.style.color = "white";
+  });
+
+  element.append(p);
+  element.append(btn);
+  element.append(btnRemove);
+  document.querySelector(".container").append(element);
+
+  i++;
+}
+
+function addNewBook() {
+  let book = new Book(
+    document.getElementById("title").value,
+    document.getElementById("author").value,
+    document.getElementById("pages").value
+  );
+  myLibrary.push(book);
+  ShowBook();
+}
